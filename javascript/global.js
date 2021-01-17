28b8fc2213e7cd6eb6547909b286db7ead2869de
Yuri Ventura <yurisotc@gmail.com>
2021-01-17 00:04:26 -0300

    Os arquivos foram modificados para se adequar ao backend que será desenvolvido.

--- javascript/global.js
+++ javascript/global.js
@@ -1,16 +1,22 @@
 
-//status 0 para display none e 1 para display block
-
-var status = 0
-
 function altera_exibicao(item_id){
     let item = document.getElementById(item_id)
-    if(status == 0){
-        item.setAttribute("style", "display: block;")
-        status = 1
+    let lista_item_visivel = document.getElementsByClassName("visivel")
+    let item_visivel
+    let i
+    if(item.classList.contains("invisivel")){
+        item.classList.remove("invisivel")
+        item.classList.add("visivel")
     }
     else{
-        item.setAttribute("style", "display: none;")
-        status = 0
+        item.classList.remove("visivel")
+        item.classList.add("invisivel")
+    }
+    for(i = 0; i < lista_item_visivel.length; i++){
+        item_visivel = document.getElementById(lista_item_visivel[i].id)
+        if(item_visivel != item){
+            item_visivel.classList.remove("visivel")
+            item_visivel.classList.add("invisivel")
+        } 
     }
 }
--- upload_arquivos.php
+++ upload_arquivos.php
@@ -2,6 +2,6 @@
 
 $arquivo = $_FILES["arquivo"];
 move_uploaded_file($arquivo["tmp_name"], $_SERVER["DOCUMENT_ROOT"].'/arquivos/'.$arquivo["name"]);
-include_once("index.html");
+include_once("index.php");
 
 ?>
